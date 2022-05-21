2d549faaf0e0a706c744452f38ac5981e01132c2
vmganela <vmganela@gmail.com>
2022-05-21 17:24:08 +0000

    Added unit test **test_get_todo_error**
    Get calls in case of error are not teste and they should be tested

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -58,7 +58,6 @@ class TestDatabaseFunctions(unittest.TestCase):
         #self.assertIn('todoTable', self.table_local.name)
         print ('End: test_table_exists')
         
-
     def test_put_todo(self):
         print ('---------------------')
         print ('Start: test_put_todo')
@@ -104,6 +103,16 @@ class TestDatabaseFunctions(unittest.TestCase):
             self.text,
             responseGet['text'])
         print ('End: test_get_todo')
+    
+    def test_get_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_get_todo_error')
+        from src.todoList import get_item
+       
+         # Table mock
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        print ('End: test_get_todo_error')
     
     def test_list_todo(self):
         print ('---------------------')
