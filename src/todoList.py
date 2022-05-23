fc531104a066624de429397839cb7b8598063770
vmganela <vmganela@gmail.com>
2022-05-23 09:55:55 +0000

    Made lines with less thant 80 chars

--- src/todoList.py
+++ src/todoList.py
@@ -13,7 +13,8 @@ def get_table(dynamodb=None):
             URL = os.environ['ENDPOINT_OVERRIDE']
             if URL:
                 print('URL dynamoDB:'+URL)
-                boto3.client = functools.partial(boto3.client, endpoint_url=URL)
+                boto3.client = functools.partial(boto3.client,
+                                                 endpoint_url=URL)
                 boto3.resource = functools.partial(boto3.resource,
                                                    endpoint_url=URL)
             dynamodb = boto3.resource("dynamodb")
